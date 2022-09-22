08ea804ed916037d43a2087a8824d99f70ae6a15
Md. Alamin Hossain <96656016+alamin486@users.noreply.github.com>
2022-09-22 16:51:32 +0600

    Add files via upload

---
+++ JS function in React/JavaScript.js
@@ -0,0 +1,14 @@
+function MyPage() {
+    return (
+        <div>
+            <img src="react.png" width="40px"></img>
+            <h1>Fun Facts about React</h1>
+            <ul>
+                <li>Was First released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k star on gitHub</li>
+            </ul>
+        </div>
+    )
+}
+ReactDOM.render(<MyPage />, document.getElementById("root"))
