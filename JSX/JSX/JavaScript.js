b8ce6d801767e3449b8d0fbf338c26abb5876d71
Md. Alamin Hossain <96656016+alamin486@users.noreply.github.com>
2022-09-22 16:51:13 +0600

    Add files via upload

---
+++ JSX/JSX/JavaScript.js
@@ -0,0 +1,12 @@
+//JSX stands for JavaScript XML. JSX allows us to write HTML in React. 
+//JSX makes it easier to write and add HTML in React.
+ReactDOM.render(
+    <nav>
+        <h1 className="header">My website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Content</li>
+        </ul>
+        </nav>,
+    document.getElementById("root"))
---
+++ JSX/JavaScript.js
@@ -0,0 +1,8 @@
+//JSX stands for JavaScript XML. JSX allows us to write HTML in React. 
+//JSX makes it easier to write and add HTML in React.
+ReactDOM.render(
+    <div>
+        <h1 className="header">This is JSX</h1>
+        <p>This is a paragraph</p>
+        </div>,
+    document.getElementById("root"))
