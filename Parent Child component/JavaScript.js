7941193e28bd8989e79896b739394d5302bb2d28
Md. Alamin Hossain <96656016+alamin486@users.noreply.github.com>
2022-09-22 16:50:27 +0600

    Add files via upload

---
+++ Parent Child component/JavaScript.js
@@ -0,0 +1,41 @@
+function Header() {
+    return (
+
+        <header>
+            <nav> <img src="react.png" width="40px"></img> </nav>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+        </header>
+    )
+}
+function Footer(){
+    return(
+        <footer>
+            <small>@ 2022 Alamin development. All right reserved.</small>
+        </footer>
+    )
+}
+function MainPage(){
+    return(<div> <h1>Fun Facts about React</h1>
+    <ol>
+        <li>Was First released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k star on gitHub</li>
+    </ol>
+    </div>)
+}
+function MyPage() {
+    return (
+
+        <div>
+            <Header />
+           <MainPage />
+           <Footer />
+
+        </div>
+    )
+}
+ReactDOM.render(<MyPage />, document.getElementById("root"))
