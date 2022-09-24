061013e0585616c034cb389515d66e8cdad20c96
Md. Alamin Hossain <96656016+alamin486@users.noreply.github.com>
2022-09-24 21:24:23 +0600

    Add files via upload

---
+++ React App -Project 2/src/component/Main.js
@@ -0,0 +1,14 @@
+import React from "react"
+import image from "./Group 82.png"
+export default function Main(){
+    return(
+        <section class="section--line">
+            <img src={image} class="Main--photo"/>
+            <h1 class="Main--header">Online experience</h1>
+            <p>Join unique interactive led by one-of-a-kind hosts-all without living Home.Join unique interactive led by one-of-a-kind hosts-all without living Home.Join unique interactive led by one-of-a-kind hosts-all without living Home.</p>
+            <p>Join unique interactive led by one-of-a-kind hosts-all without living Home.Join unique interactive led by one-of-a-kind hosts-all without living Home.</p>
+
+            
+        </section>
+    )
+}
